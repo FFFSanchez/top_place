63b719f7ea52d11a3c5b8ebc58908f477f937880
Alexandr Trifonov <lordsanchez@yandex.ru>
2023-03-07 20:07:25 +0400

    TEMPLATES_DIR и STATIC_URL в settings

--- top_place/top_place/settings.py
+++ top_place/top_place/settings.py
@@ -57,10 +57,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'top_place.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
