a65629962ced1571d002945c26b396b19a02a0a0
Alexandr Trifonov <lordsanchez@yandex.ru>
2023-03-07 22:02:19 +0400

    добавил остальные страницы шаблоны и namespace ссылки

--- top_place/blog/urls.py
+++ top_place/blog/urls.py
@@ -7,4 +7,8 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+    path('articles/', views.articles, name='articles'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('project/', views.project, name='project'),
 ]
--- top_place/blog/views.py
+++ top_place/blog/views.py
@@ -15,3 +15,35 @@ def contact(request):
         'text': 'Кружочки'
     }
     return render(request, template, context)
+
+
+def services(request):
+    template = 'blog/services.html'
+    context = {
+        'text': 'Кружочки'
+    }
+    return render(request, template, context)
+
+
+def articles(request):
+    template = 'blog/articles.html'
+    context = {
+        'text': 'Кружочки'
+    }
+    return render(request, template, context)
+
+
+def portfolio(request):
+    template = 'blog/portfolio.html'
+    context = {
+        'text': 'Кружочки'
+    }
+    return render(request, template, context)
+
+
+def project(request):
+    template = 'blog/project.html'
+    context = {
+        'text': 'Кружочки'
+    }
+    return render(request, template, context)
