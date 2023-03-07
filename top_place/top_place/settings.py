45a94cf826ebbdeb18db552b7ae7c86d0b8934fa
Alexandr Trifonov <lordsanchez@yandex.ru>
2023-03-07 19:51:12 +0400

    добавлен .env и туда SECRET KEY

--- top_place/top_place/settings.py
+++ top_place/top_place/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lrtih_57s$)8&!7azejrue=f0%sk6v5(r$)s==s-u46-u%i2jx'
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
