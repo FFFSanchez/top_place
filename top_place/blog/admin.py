35c4b38d2125965de3115bbbfcb66dfb84f7ed1b
Alexandr Trifonov <lordsanchez@yandex.ru>
2023-03-07 23:30:30 +0400

    модели статьи, группы, миграции, суперюзер и пару статей

--- top_place/blog/admin.py
+++ top_place/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Group
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+    list_editable = ('group',)
+
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Group)
--- top_place/blog/models.py
+++ top_place/blog/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Article(models.Model):
+    text = models.TextField(
+        'Текст статьи',
+        help_text='Введите текст статьи'
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='articles',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        'Group',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='articles',
+        verbose_name='Группа',
+        help_text='Группа, к которой будет относиться статья'
+    )
+    # Поле для картинки (необязательное)
+    #image = models.ImageField(
+    #     'Картинка',
+    #     upload_to='posts/',
+    #     blank=True,
+    #     null=True,
+    # )
+    # Аргумент upload_to указывает директорию,
+    # в которую будут загружаться пользовательские файлы.
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ('-pub_date',)  # критерии сортировки
+
+    def __str__(self):
+        # выводим текст поста
+        return self.text[:15]
+
+
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.title
