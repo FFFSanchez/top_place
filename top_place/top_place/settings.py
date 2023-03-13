58f9f55169a5aac45bc6f59e0bfd3bb32ebf4f0e
Alexandr Trifonov <lordsanchez@yandex.ru>
2023-03-13 13:32:03 +0400

    починил регистрацию, автологин теперь, и сброс пароля работает через мой смтп благодяра DEFAULT_FROM_EMAIL = EMAIL_HOST_USER

--- top_place/top_place/settings.py
+++ top_place/top_place/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
-from django.urls import reverse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,3 +149,6 @@ LOGIN_REDIRECT_URL = '/0'
 # reverse(
 #     'blog:index', kwargs={'poll_id': 0}
 # )
+
+# для pass reset, только так работает но работает збс
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- top_place/users/views.py
+++ top_place/users/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 # Импортируем класс формы, чтобы сослаться на неё во view-классе
 from .forms import CreationForm
@@ -8,5 +10,16 @@ from .forms import CreationForm
 class SignUp(CreateView):
     form_class = CreationForm
     # После успешной регистрации перенаправляем пользователя на главную.
-    success_url = reverse_lazy('blog:index')
+
+    # 'users:login'
+    success_url = reverse_lazy('blog:index', args=[0])
     template_name = 'users/signup.html'
+
+    def form_valid(self, form):
+        """If the form is valid,
+        save the associated model and log the user in."""
+
+        user = form.save()
+        login(self.request, user)
+
+        return redirect(self.success_url)
